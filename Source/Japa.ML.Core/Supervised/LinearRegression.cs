f217b495460dcfca616bb6e59dda3d7e879b6c2c
guilhermeando <japa.studios@live.com>
2014-06-02 10:18:13 -0300

    initial

---
+++ Source/Japa.ML.Core/Supervised/LinearRegression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Japa.ML.Core.Supervised
+{
+    public class LinearRegression
+    {
+        private void Cost() 
+        { 
+        
+        }
+        private void Gradient()
+        {
+
+        }
+
+        public void Initialize() 
+        { 
+
+        }
+
+        public void Train()
+        { 
+        
+        }
+
+
+    }
+}
---
+++ Tests/Japa.ML.Core.Tests/NeuralNetworkTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Japa.ML.Core.Supervised;
+
+namespace Japa.ML.Core.Tests
+{
+    [TestClass]
+    public class NeuralNetworkTests
+    {
+        [TestMethod]
+        public void Training()
+        {
+            var nn = new NeuralNetwork(new NeuralNetworkConfig
+            {
+
+            });
+            nn.Train(new NeuralNetworkTrainRequest { });
+        }
+    }
+}
